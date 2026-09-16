c6ee392ef294e02ea6e44e2e515fb3fa0f655ad5
sheraz-ali <soomrosheraz054@gmail.com>
2026-09-16 14:27:36 +0500

    Tennish Match Booking app - Project init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tennis Match Booking',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(body: Center(child: Text("Match Booking app"),),)
+    );
+  }
+}
+
